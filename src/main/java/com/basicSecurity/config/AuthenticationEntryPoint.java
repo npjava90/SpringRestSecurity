312a9440abf26062379eadb17c95a66101e846ca
Nisarg Pathak <npjava90@gmail.com>
2018-04-12 10:45:46 +0530

    Update AuthenticationEntryPoint.java

--- src/main/java/com/basicSecurity/config/AuthenticationEntryPoint.java
+++ src/main/java/com/basicSecurity/config/AuthenticationEntryPoint.java
@@ -24,7 +24,7 @@ public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 	
 	@Override
     public void afterPropertiesSet() throws Exception {
-        setRealmName("DeveloperStack");
+        setRealmName("npp");
         super.afterPropertiesSet();
     }
 
